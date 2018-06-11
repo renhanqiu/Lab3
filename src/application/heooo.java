fe9ad7bcdafa93784e24cd1ad25864db4ffc5e8f
renhanqiu <1319699804@qq.com>
2018-06-11 21:36:22 +0800

    aaa

--- src/application/heooo.java
+++
@@ -1,7 +0,0 @@
-package application;
-
-public class heooo {
-public static void main(String[] args) {
-	System.out.println("sjkdfs");
-}
-}
--- src/application/hhh.java
+++ src/application/hhh.java
@@ -3,5 +3,6 @@ package application;
 public class hhh {
 public static void main(String[] args) {
 	System.out.println("sjdfs");
+	System.out.println("sjdfsdsdfsdfsd");;
 }
 }
